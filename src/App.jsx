df513334ea3fa35aaf1a7cfffc9fe3198d5aa834
Harsh Jain <hkankariya19@gmail.com>
2023-11-02 19:59:29 +0530

    adding some properties of react and in deatails of estimated variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@
 function App() {
  
 
+const username="chai aur code"  // if we write variable  in curly bracket it will be act as variable 
   return (
-   <h1>Chai Aur React with vite | Harsh Kankariya</h1>
+
+    // here only evaluated expression is used like username 
+   <h1>Chai Aur React with vite | Harsh Kankariya learning with {username} </h1>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,57 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Add from './Add.jsx'
 
+function Myfun()
+{
+     return(
+        <h1>Myself Harsh Kumar Kakariya</h1>
+     )
+}
+
+
+// It will not work directly on this because it is in random order which will unable read by react library
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'click me to visit google'
+// }
+
+
+const anotherElement=(
+        <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+// creating react Element in this order
+const anotheruser="  chai aur code"   // evaluated expression
+const reactElement=React.createElement(
+    'a',
+    {href:"https://google.com",target:'_blank'},
+    "click me to visit google",
+    anotheruser
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
    
     // <></>  it is used to bind when we have to run or return more than one element
-    <>  
-    <App />   
-    <Add />
-   </>
+//     <>  
+    // <App />   
+    // <Add />
+//    </>
+
+// we can also run directly any functin in this
+//  <Myfun /> 
+  
+// it will not work
+// reactElement
+
+   // if we pass a const in this order it will directly work
+   //    anotherElement
+
+// paritcular method to make element in react
+reactElement
+
 )
